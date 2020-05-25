4482c2b00117ce4f61885c3922e16ced20399f08
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-25 08:55:34 +0000

    devise複数作成、enum作成、association

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,14 +1,6 @@
-class User < ApplicationRecord
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  belongs_to :about
-  has_many :articles, dependent: :destroy
-
-  enum rate: {
-       men: 1,
-       women: 2
-  }#カラム名   #定義対象の内容のマッピング(名前: 数値)
 end
--- db/migrate/20200525072703_devise_create_users.rb
+++ db/migrate/20200525072703_devise_create_users.rb
@@ -31,8 +31,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-
+      t.string :first_name
+      t.string :last_name
+      t.string :kana_first_name
+      t.string :kana_last_name
+      t.integer :sex
+      t.text :introduction
+      t.string :profile_image_id
       t.timestamps null: false
     end
 
