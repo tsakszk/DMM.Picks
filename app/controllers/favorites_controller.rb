063d1e1458c86b6e57b3072b4ebe4a5d18841c50
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-25 09:18:20 +0000

    user側のコントローラー作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+  def follows
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def followers
+  end
+
+  def index
+  end
+
+  def list
+  end
+end
