3fe7e97529a34295758625cca4188b7206eefa2a
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-25 09:44:00 +0000

    devose以外のcontroller作成

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,13 @@
+class Admins::UsersController < ApplicationController
+  def top
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,43 @@
 Rails.application.routes.draw do
 
+  namespace :admins do
+    get 'articles/index'
+    get 'articles/create'
+    get 'articles/edit'
+    get 'articles/update'
+    get 'articles/show'
+    get 'articles/new'
+  end
+  namespace :admins do
+    get 'categorys/index'
+    get 'categorys/create'
+    get 'categorys/edit'
+    get 'categorys/update'
+  end
+  namespace :admins do
+    get 'hamburgers/index'
+    get 'hamburgers/create'
+    get 'hamburgers/edit'
+    get 'hamburgers/update'
+  end
+  namespace :admins do
+    get 'abouts/top'
+    get 'abouts/index'
+    get 'abouts/show'
+    get 'abouts/update'
+  end
+  namespace :admins do
+    get 'users/top'
+    get 'users/index'
+    get 'users/edit'
+    get 'users/update'
+  end
+  namespace :admin do
+    get 'users/top'
+    get 'users/index'
+    get 'users/edit'
+    get 'users/update'
+  end
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
     passwords:     'admins/passwords',
