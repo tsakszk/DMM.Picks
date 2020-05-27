0b79760cc48427f1126c39bc6d6522d8a4833ac4
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-27 10:25:51 +0000

    seeds_fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,30 +57,3 @@ Admin.create!(
   password: 'password',
   password_confirmation: 'password'
 )
-=======
-
-# users table
-first_name
-last_name
-kana_first_name
-kana_last_name
-email
-sex
-
-#categorys
-(people,culture,other)
-
-
-# hamburgers
-hamburgers_name
-(mypicks,source,setting,about,logout)
-
-
-
-
-# admin_users
-AdminUser.create!(
-    email: "admin@example.com",
-    password: 'password',
-    password_confirmation: 'password'
-  )
