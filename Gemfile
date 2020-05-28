373554907482d3c5eb9acde4df22a21607964b54
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-28 09:14:19 +0000

    devise_Start over

--- Gemfile
+++ Gemfile
@@ -95,3 +95,4 @@ gem 'dotenv-rails'
 group :production do
   gem 'mysql2'
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'public/users#index'
+  devise_for :users
+  # devise_for :users　#deviseを使用する際にURLとしてusersを含むことを示す。どのようなルーティングになるのか、rails routesを実行して確認
+  # root 'public/users#index'
 
 end
