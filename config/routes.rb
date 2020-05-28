8b424efb10ac864eb451cf973f7278dc7c9f7a08
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-28 11:00:14 +0000

    devise_fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-  # devise_for :users　#deviseを使用する際にURLとしてusersを含むことを示す。どのようなルーティングになるのか、rails routesを実行して確認
-  # root 'public/users#index'
+  devise_for :users#deviseを使用する際にURLとしてusersを含むことを示す。どのようなルーティングになるのか、rails routesを実行して確認
 
 end
