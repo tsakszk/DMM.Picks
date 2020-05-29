2049f23473994cb3940bcc1d4409aed8a919c478
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-29 07:45:34 +0000

    cp

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,37 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable,:validatable
+  has_many :books,dependent: :destroy
   attachment :profile_image, destroy: false
+  #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
+  validates :name, presence: true
+  validates :name, {length: {in: 2..20}}
+  validates :introduction, {length: {maximum: 50 }}
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :following_user, through: :follower, source: :followed # 自分がフォローしている人　フォロー関係を持っている人
+  has_many :follower_user, through: :followed, source: :follower # 自分をフォローしている人　フォロワー関係を持っている人
+
+# ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォローしていればtrueを返す
+  def following?(user)
+    following_user.include?(user)
+  end
+
+  def follower?(user)
+    follower_user.include?(user)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_084317) do
+ActiveRecord::Schema.define(version: 2020_05_29_073926) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
