24704071443a8cfc6face195b641716d0faf450a
toshiaki suzuki <toshiaki1986@gmail.com>
2020-05-29 07:10:21 +0000

    books&usersコントローラ 、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users#deviseを使用する際にURLとしてusersを含むことを示す。どのようなルーティングになるのか、rails routesを実行して確認
+  devise_for :users   #deviseを使用する際にURLとしてusersを含むことを示す。どのようなルーティングになるのか、rails routesを実行して確認
   resources :users
-
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   root 'home#top'
   get 'home/about'
 
