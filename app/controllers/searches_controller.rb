0285578dea749c4800b535b9f5835ebcf7bae70e
toshiaki suzuki <toshiaki1986@gmail.com>
2020-06-02 09:57:06 +0000

    ヘッダー調整、サーチ機能作成途中

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,10 @@
 class SearchesController < ApplicationController
   def search
-    if params[:search].present?
-      @books = Book.where(title: params[:search])
+    if params[:search].present?  #Railsにおいてpresentメソッドはオブジェクトであるレシーバーの値が存在すればtrue、存在しなければfalseを返すメソッド。
+      @books = Book.where(title: params[:search], body: params[:search])
     else
       @books = Book.all
     end
   end
 end
+
